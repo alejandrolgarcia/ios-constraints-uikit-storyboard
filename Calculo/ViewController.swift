054c6ce6c47b48c330cec9c8928e58717976dd93
Luis Garcia <lalejandro.1988@gmail.com>
2021-12-27 21:53:19 -0800

    Deseño: uso de Constraints

--- Calculo/ViewController.swift
+++ Calculo/ViewController.swift
@@ -11,7 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Observable que esta pendiente de cuando ejecutamos cierta accion, por ejemplo abrir el teclado
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(tecladoDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func tecladoUp() {
+        // Obtener valor en pixeles de la pantalla del dispositivo
+        let sizeScreen = UIScreen.main.nativeBounds.height
+        
+        if sizeScreen == 1136 {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y = -50
+            }
+        }
+    }
+    
+    @objc func tecladoDown(){
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    // Ocultar el teclado al dar clic en el boton
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
 
