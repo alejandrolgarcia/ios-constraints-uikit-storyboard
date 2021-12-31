127bcad6d828f628075433886e295c23e7e62036
Luis Garcia <lalejandro.1988@gmail.com>
2021-12-30 22:28:43 -0800

    programacion de botones

--- Calculo/ViewController.swift
+++ Calculo/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var resultado: UILabel!
+    @IBOutlet weak var resultadoDescuento: UILabel!
+    @IBOutlet weak var cantidadtxt: UITextField!
+    @IBOutlet weak var porcentajetxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,6 +45,27 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
 
-
+    @IBAction func calcular(_ sender: UIButton) {
+        guard let cantidad = cantidadtxt.text else { return }
+        guard let porcentaje = porcentajetxt.text else { return }
+        
+        let cant = (cantidad as NSString).floatValue
+        let porciento = (porcentaje as NSString).floatValue
+        
+        let desc = (cant * porciento)/100
+        let res = cant - desc
+        
+        resultado.text = "$\(res)"
+        resultadoDescuento.text = "$\(desc)"
+        self.view.endEditing(true)
+        
+    }
+    
+    @IBAction func limpiar(_ sender: UIButton) {
+        cantidadtxt.text = ""
+        porcentajetxt.text = ""
+        resultado.text = "$0.00"
+        resultadoDescuento.text = "$0.00"
+    }
 }
 
